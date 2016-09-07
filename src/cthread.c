9f37d89cfb8800a60a30b9261ba188f272bb488e
Luís Möllmann <luisgustavo181@gmail.com>
2016-09-07 16:43:49 -0300

    first rough sketch, seems to work

--- src/cthread.c
+++ src/cthread.c
@@ -1,5 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <ucontext.h>
 #include "support.h"
+#include "cthread.h"
+
+#define VERBOSE 1
 
 #define ST_CREATION 0
 #define ST_APT 1
@@ -15,36 +20,93 @@ typedef struct s_TCB {
     ucontext_t context;
 } TCB_t;
 
+/***** catalogging */
+
 /* temporary TCB 'tree' */
 static TCB_t threads[50];
 
-TCB_t *getTCB(int tid) {
+TCB_t getTCB(int tid) {
     return threads[tid];
 }
+
+/* put 'tcb' in catalog */
+static void keepTCB(TCB_t tcb) {
+    threads[tcb.tid] = tcb;
+}
+
+/***** TCB manipulation and utils */
+
+/* return initialized TCB, insert it to the TCB "catalog" */
+static TCB_t TCB_init(int tid) {
+    TCB_t thr;
+    char *stack = malloc(1024*sizeof(char));
+
+    thr.tid = tid;
+    thr.state = ST_APT;
+    thr.ticket = tid;
+    getcontext(&(thr.context));
+    thr.context.uc_stack.ss_sp = stack;
+    thr.context.uc_stack.ss_size = sizeof(stack);
+
+    keepTCB(thr); /* insert in catalog */
+
+    return thr;
+}
+
+static void printTCB(int tid) {
+    if (VERBOSE) {
+        TCB_t thr = getTCB(tid);
+        const char *state[5] = {"creation", "apt", "executing", "blocked", "ended"};
+        printf("tid: %d\n", thr.tid);
+        printf("state: %s\n", state[thr.state]);
+        printf("ticket: %d\n", thr.ticket);
+    }
+}
+
+/***** interface */
 
 int ccreate (void* (*start)(void*), void *arg) {
     static last_tid = -1;
 
+    /* check for main */
     if (last_tid == -1) {
-        TCB_t main = (TCB_t *)malloc(sizeof(TCB_t));
+        TCB_t main;
 
-        main.tid = last_tid + 1;
-        last_tid++;
+        main = TCB_init(++last_tid);
+        main.context.uc_link = NULL;
 
-        main.state = ST_RUNNING;
-
-        main.ticket = main.tid;
-
-        getcontext(&(main.context));
-        main.context->uclink = NULL;
-
-        threads[main.tid] = main;
+        printTCB(main.tid);
     }
+
+    TCB_t thr;
+
+    thr = TCB_init(++last_tid);
+    //thr.context.uc_link = /* to dispatcher */
+    printTCB(thr.tid);
+
+    return 0;
 }
 
-//int cyield(void);
-//int cjoin(int tid);
-//int csem_init(csem_t *sem, int count);
-//int cwait(csem_t *sem);
-//int csignal(csem_t *sem);
-//int cidentify (char *name, int size);
+int cyield(void) {
+    return -1;
+}
+
+int cjoin(int tid) {
+    return -1;
+}
+
+int csem_init(csem_t *sem, int count) {
+    return -1;
+}
+
+int cwait(csem_t *sem) {
+    return -1;
+}
+
+int csignal(csem_t *sem) {
+    return -1;
+}
+
+int cidentify (char *name, int size) {
+    return -1;
+}
---
+++ testes/test_ccreate.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include "support.h"
+#include "cthread.h"
+
+int main(int argc, char** argv) {
+    ccreate(NULL, NULL);
+    return 0;
+}
