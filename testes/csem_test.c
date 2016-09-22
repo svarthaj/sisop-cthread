c4737b37cfe342c165125f8e7be4df925166ccc7
Lucas Leal <lucas.leal@ufrgs.br>
2016-09-21 21:56:00 -0300

    add loginfo on csem example for debugging

--- testes/csem_test.c
+++ testes/csem_test.c
@@ -3,14 +3,27 @@
 #include "cthread.h"
 
 int main(int argc, char** argv) {
+    char info[1024];
     csem_t sem1;
 
     csem_init(&sem1, 3);
 
     cwait(&sem1);
+    sprintf(info, "sem count: %d", sem1.count);
+    loginfo(info);
+    
     cwait(&sem1);
+    sprintf(info, "sem count: %d", sem1.count);
+    loginfo(info);
+    
     cwait(&sem1);
-
+    sprintf(info, "sem count: %d", sem1.count);
+    loginfo(info);
+    
+    cwait(&sem1);
+    sprintf(info, "sem count: %d", sem1.count);
+    loginfo(info);
+    
     csignal(&sem1);
 
     return 0;
