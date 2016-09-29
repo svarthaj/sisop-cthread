458982f2e32df42a4a96df7e6f41fa7fc991a7b0
Lucas Leal <lucas.leal@ufrgs.br>
2016-09-29 17:53:52 -0300

    add cidentify test

---
+++ teste/cidentify.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "support.h"
+#include "cthread.h"
+
+
+int main(int argc, char** argv) {
+    char name[256];
+
+    cidentify(name, 255);
+
+    puts(name);
+    
+    return 0;
+}
